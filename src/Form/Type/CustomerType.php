ec40d0764b9f31c50ca7023a9d28a46f2afb779f
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-07-13 13:00:08 -0300

    Add Customer FormType

---
+++ src/Form/Type/CustomerType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotNull;
+
+class CustomerType extends AbstractType{
+    public function buildForm(FormBuilderInterface $builder, array $options):void{
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotNull(),
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'constraints' => [
+                    new NotNull(),
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver):void{
+        $resolver->serDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
