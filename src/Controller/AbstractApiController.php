932cd3424e38c3fd20a723a0c31a3bfaa15000b1
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-07-16 12:35:27 -0300

    Creating abstract API Controller

---
+++ src/Controller/AbstractApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class AbstractApiController extends AbstractController
+{
+    protected function buildForm(string $type, $data = null, array $options = []){
+        $options - array_merge($options, [
+            'csrf_protection'=>false,
+        ]);
+        return $this->container->get('form.factory')->createNamed($type, $data, $options);
+    }
+}
