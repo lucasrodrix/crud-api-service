3bfe3fe6d0823594fa47e923882b655095e48e5c
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2021-07-19 11:26:12 -0300

    Finish CRUD Cart and methods delete and update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -9,10 +9,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class CartController extends AbstractApiController
-{
-    public function showAction(Request $request): Response
-    {
+class CartController extends AbstractApiController{
+    public function showAction(Request $request): Response{
         $customerId = $request->get('id');
         
         $customer = $this->getDoctrine()->getRepository(persistentObject: Customer::class)->findOneBy(['id' => $customerId]);
@@ -40,6 +38,57 @@ class CartController extends AbstractApiController
         }
 
         /**@var Cart $cart */
+        $cart = $form->getData();
+
+        $this->getDoctrine()->getManager()->persist($cart);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->respond($cart);
+    }
+
+    public function deleteAction(Request $request): Response{
+        $cartId = $request->get('cartId');
+        $customerId = $request->get('customerId');
+
+        $cart = $this->getDoctrine()->getRepository(persistentObject: Cart::class)->findOneBy([
+            'customer' => $customerId,
+            'id' => $cartId
+        ]);
+
+        if(!$cart){
+            throw new NotFoundHttpException('Cart is not found');
+        }
+        
+        $this->getDoctrine()->getManager()->remove($cart);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->respond(null);
+    }
+
+   
+    public function updateAction(Request $request): Response
+    {
+        $customerId = $request->get('customerId');
+
+        $customer = $this->getDoctrine()->getRepository(Customer::class)->findOneBy(['id' => $customerId]);
+
+        if (!$customer) {
+            throw new NotFoundHttpException('Customer not found');
+        }
+
+        $cart = $this->getDoctrine()->getRepository(Cart::class)->findOneBy(['customer' => $customer]);
+
+        $form = $this->buildForm(CartType::class, $cart, [
+            'method' => $request->getMethod(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $this->respond($form, Response::HTTP_BAD_REQUEST);
+        }
+
+        /** @var Cart $cart */
         $cart = $form->getData();
 
         $this->getDoctrine()->getManager()->persist($cart);
